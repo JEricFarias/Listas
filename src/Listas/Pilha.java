4a76c49f7db4efce548eb7b47b031c3e070d0fa4
JEricFarias <ericfarrias@hotmail.com>
2016-02-23 01:54:07 -0300

    Modificações feitas, consegui resolver parcialmente o problema

--- src/Listas/Pilha.java
+++ src/Listas/Pilha.java
@@ -13,21 +13,21 @@ package Listas;
 public class Pilha {
     private int []array;
     private int index;
-    private int max;
-    private int min;
+    private int maxValue;
+    private int minValue;
     
     public Pilha(){
         this.array = new int[16];
         this.index = 0;
-        this.max = Integer.MIN_VALUE;
-        this.min = Integer.MAX_VALUE;
+        this.maxValue = Integer.MIN_VALUE;
+        this.minValue = Integer.MAX_VALUE;
     }
     
     public Pilha(int size){
         this.array = new int[size];
         this.index = 0;
-        this.max = Integer.MIN_VALUE;
-        this.min = Integer.MAX_VALUE;
+        this.maxValue = Integer.MIN_VALUE;
+        this.minValue = Integer.MAX_VALUE;
     }
     
     public void add(int valor){
@@ -35,11 +35,11 @@ public class Pilha {
         //o valor pode assumir valores negativos, não? 2 ponto: falta validar: valor < 0 || valor > array.length (AIOBEx)
         if(this.index < this.array.length){
             this.array[this.index] = valor;
-            if(valor > this.max){
-                this.setMax(valor);
+            if(valor > this.maxValue){
+                this.setMaxValue(valor);
             }
-            if(valor < this.min){
-                this.setMin(valor);
+            if(valor < minValue){
+                this.setMinValue(valor);
             }
             this.index++;
         }else{
@@ -54,19 +54,33 @@ public class Pilha {
         this.index--;
         return valor;
     }
-    /*
-        Retirei os metodos max e min, mas agora se o valor max ou min forem retirados não tem com atualizar o novo valor.
-        So se eu deixar os metodos max e min private e usar sempre ocorrer um remove.
-     */
+    
     public int primeiroValor(){
         return this.array[0];
     }
     
-    public void setMax(int v){
-        this.max = v;
+    public void setMaxValue(int v){
+        this.maxValue = v;
     }
     
-    public void setMin(int v){
-        this.min = v;
+    public void setMinValue(int v){
+        this.minValue = v;
+    }
+    
+    /*
+        Retirei os metodos max e min, mas agora se o valor max ou min forem retirados não tem com atualizar o novo valor.
+        Então reiniciei os valores, criei um metodo update que busca na pilha so ate o indice e preenche os dois atributos.
+     */
+    private void update(){
+        this.setMaxValue(Integer.MIN_VALUE);
+        this.setMinValue(Integer.MAX_VALUE);
+        for(int i = 0; i < this.index; i++){
+            if(this.array[i] > this.maxValue){
+                this.setMaxValue(this.array[i]);
+            }
+            if(this.array[i] < this.minValue){
+                this.setMinValue(this.array[i]);
+            }
+        }
     }
 }
