67d3e04c9cd30a3daf76e5e7073ee13bb2896213
JEricFarias <ericfarrias@hotmail.com>
2016-02-27 04:16:23 -0300

    Classe de test adicionada e bugs corrigidos

--- src/Listas/Pilha.java
+++ src/Listas/Pilha.java
@@ -19,7 +19,7 @@ public class Pilha {
     
     public Pilha(){
         this.array = new int[16];
-        this.index = 0;
+        this.index = -1;
         // quando o objeto é criado o maior valor esta no array indice 0(zero);
         this.indexMaxValue = 0;
         // quando o objeto é criado o maior valor esta no array indice 0(zero);
@@ -32,7 +32,7 @@ public class Pilha {
         }else{
             throw new NegativeArraySizeException("Tamnho de arrays tem que ser maior que zero");
         }
-        this.index = 0;
+        this.index = -1;
         // quando o objeto é criado o maior valor esta no array indice 0(zero);
         this.indexMaxValue = 0;
         // quando o objeto é criado o maior valor esta no array indice 0(zero);
@@ -40,15 +40,15 @@ public class Pilha {
     }
     
     public void add(int valor){
+        this.index++;
         if(this.index < this.array.length){
             this.array[this.index] = valor;
-            if(valor > this.indexMaxValue){
+            if(valor > this.array[this.indexMaxValue]){
                 this.setIndexMaxValue(this.index);
             }
-            if(valor < indexMinValue){
+            if(valor < this.array[this.indexMinValue]){
                 this.setIndexMinValue(this.index);
             }
-            this.index++;
         }else{
             // add método para almentar dinamicamente o array
             throw new StackOverflowError("A operação não pode ser realizada pois a pilha encontra-se cheia!");
@@ -92,21 +92,29 @@ public class Pilha {
         this.indexMinValue = v;
     }
     
+    public int getStackSize(){
+        return this.array.length;
+    }
+    
     /*
         Retirei os metodos max e min, mas agora se o valor max ou min forem retirados não tem com atualizar o novo valor.
         Então reiniciei os valores, criei um metodo update que busca na pilha so ate o indice e preenche os dois atributos.
      */
     private void updateIndexMaxValue(){
-        for(int i = 0; i < --this.index; i++){
-            if(this.array[i] > this.array[this.indexMaxValue]){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < this.index; i++){
+            if(this.array[i] > max){
+                max = this.array[i];
                 this.setIndexMaxValue(i);
             }
         }
     }
     
     private void updateIndexMinValue(){
-        for(int i = 0; i < --this.index; i++){
-            if(this.array[i] < this.array[this.indexMinValue]){
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < this.index; i++){
+            if(this.array[i] < min){
+                min = this.array[i];
                 this.setIndexMinValue(i);
             }
         }
---
+++ test/Listas/PilhaTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author eide
+ */
+public class PilhaTest {
+    
+    @Test(expected = NegativeArraySizeException.class)
+    public void testVetorDeTamanhoNegativo(){
+        Pilha p = new Pilha(-3);
+    }
+    
+    @Test
+    public void testVatorComTamanhoValido(){
+        Pilha p = new Pilha(3);
+        Assert.assertEquals(3, p.getStackSize());
+    }
+    
+    @Test(expected = StackOverflowError.class)
+    public void testEstouroDePilha(){
+        Pilha p = new Pilha(10);
+        for(int i = 0; i < 11; i++){
+            p.add(23);
+        }
+    }
+    
+    @Test
+    public void testValorMaximo(){
+        Pilha p = new Pilha(10);
+        p.add(1);
+        p.add(2);
+        p.add(1000);
+        p.add(30);
+        p.add(500);
+        
+        Assert.assertEquals(1000,p.getMaxValue());
+    }
+    
+    @Test
+    public void testValorMinimo(){
+        Pilha p = new Pilha(10);
+        p.add(1);
+        p.add(2);
+        p.add(1000);
+        p.add(30);
+        p.add(500);
+        
+        Assert.assertEquals(1,p.getMinValue());
+    }
+    
+    @Test
+    public void testTrocaMaiorValor(){
+        Pilha p = new Pilha(10);
+        p.add(1);
+        p.add(2);
+        p.add(1000);
+        p.add(30);
+        p.add(5000);
+        
+        System.out.println("Listas.PilhaTest.testTrocaMaiorValor() --> Valor Removido = " + p.remove());
+        
+        Assert.assertEquals(1000,p.getMaxValue());
+    }
+    
+    @Test
+    public void testTrocaDoMenorValor(){
+        Pilha p = new Pilha(10);
+        p.add(7);
+        p.add(2);
+        p.add(1000);
+        p.add(30);
+        p.add(500);
+        p.add(1);
+        
+        p.remove();
+        
+        Assert.assertEquals(2,p.getMinValue());
+    }
+}
